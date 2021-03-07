105713d2e13b741b0a7207102415867341405fa7
Wenhao Huang <st_hwh@yahoo.co.jp>
2021-03-07 19:55:24 +0900

    add json data interfaces

--- src/DataServlet.java
+++ src/DataServlet.java
@@ -1,12 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.UUID;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
  
 public class DataServlet extends HttpServlet {
@@ -16,30 +21,90 @@ public class DataServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		//JSON Format
 		request.setCharacterEncoding("UTF-8");
 	    response.setContentType("text/html;charset=UTF-8");
 	    String acceptjson = "";
+	    //SQL Connection
+	    String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+		String dbURL = "jdbc:sqlserver://192.168.3.102:1433;DatabaseName=****";
+		String userName = "sa";
+		String userPwd = "********";
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String time = new SimpleDateFormat("hh:mm:ss").format(new Date());
+        String logAPI = "DataServlet";
+        String log = date + " " + time + "-" + logAPI + "-";
+		Connection dbConn = null;
+		ResultSet rs = null;
+		try
+		{
+			Class.forName(driverName);
+			dbConn = DriverManager.getConnection(dbURL, userName, userPwd);
+			System.out.println(log+"Database Connect Success!");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.out.print(log+"Err:Database Connect Failure with "+e.toString());
+
+		}
 	    try {
 	        BufferedReader br = new BufferedReader(new InputStreamReader((ServletInputStream) request.getInputStream(), "utf-8"));
 	        StringBuffer sb = new StringBuffer("");
 	        String temp;
+	        String uuid[] = new String[100];
+	        String timeline[] = new String[100];
+	        String sensorid[] = new String[100];
+	        String sensignal[] = new String[100];
+	        String senbattery[] = new String[100];
+	        String sentemp[] = new String[100];
+	        String senhumi[] = new String[100];
+	        String senco2[] = new String[100];
+	        String passengers[] = new String[100];
+	        String busnumber[] = new String[100];
 	        while ((temp = br.readLine()) != null) {
 	            sb.append(temp);
 	        }
 	        br.close();
 	        acceptjson = sb.toString();
 	        JSONObject raw_JsonData = JSONObject.parseObject(acceptjson);
-	        
-	        
-	        
-	        
+	        JSONArray JSensorDataArray = raw_JsonData.getJSONArray("sensor");
+	        for(int i=0;i<JSensorDataArray.size();i++) {
+	        	JSONObject JSenseDataObject = null;
+	        	JSenseDataObject = JSensorDataArray.getJSONObject(i);
+	        	uuid[i] = UUID.randomUUID().toString();
+	        	timeline[i] = JSenseDataObject.getString("timeline");
+	        	sensorid[i] = JSenseDataObject.getString("sensorid");
+	        	sensignal[i] = JSenseDataObject.getString("sensignal");
+	        	senbattery[i] = JSenseDataObject.getString("senbattery");
+	        	sentemp[i] = JSenseDataObject.getString("sentemp");
+	        	senhumi[i] = JSenseDataObject.getString("senhumi");
+	        	senco2[i] = JSenseDataObject.getString("senco2");
+	        	passengers[i] = JSenseDataObject.getString("passengers");
+	        	busnumber[i] = JSenseDataObject.getString("busnumber");
+	        	String sql_co2 = "insert into data_co2sensors (uuid,timeline,sensorid,sensignal,senbattery,sentemp,senhumi,senco2) values ('"+uuid[i]+"','"+timeline[i]+"','"+sensorid[i]+"','"+sensignal[i]+"','"+senbattery[i]+"','"+sentemp[i]+"','"+senhumi[i]+"','"+senco2[i]+"')";
+	        	System.out.println(log+sql_co2);
+	        	PreparedStatement stmt_co2 = dbConn.prepareStatement(sql_co2);
+	        	stmt_co2.executeUpdate();
+	        	String sql_passenger = "insert into data_buspassenger (uuid,timeline,busnumber,passenger) "
+	        			+ "values ('"+uuid[i]+"','"+timeline[i]+"','"+busnumber[i]+"','"+passengers[i]+"')";
+	        	System.out.println(log+sql_passenger);
+	        	PreparedStatement stmt_passenger = dbConn.prepareStatement(sql_passenger);
+	        	stmt_passenger.executeUpdate();
+	        }
 	        String jsonStr = "{\"status\":\"success\"}";
 	        response.getWriter().write(jsonStr);
 	    } catch (Exception e) {
 	    	String jsonStr = "{\"status\":\"error\"}";
 	    	response.getWriter().write(jsonStr);
 	        e.printStackTrace();
+	    } finally{
+	    	try {
+				dbConn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	    }
-	System.out.println(acceptjson);
 	}
 }
---
+++ src/SearchBusServlet.java
@@ -0,0 +1,128 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URLDecoder;
+import java.util.UUID;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+ 
+public class SearchBusServlet extends HttpServlet {
+	/**
+	 * Do the request of search from web page
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		//JSON Format
+		request.setCharacterEncoding("UTF-8");
+	    response.setContentType("text/html;charset=UTF-8");
+	    String acceptjson = "";
+	    //SQL Connection
+	    String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+		String dbURL = "jdbc:sqlserver://192.168.3.102:1433;DatabaseName=***";
+		String userName = "sa";
+		String userPwd = "********";
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String time = new SimpleDateFormat("hh:mm:ss").format(new Date());
+        String logAPI = "SearchBusServlet";
+        String log = date + " " + time + "-" + logAPI + "-";
+		Connection dbConn = null;
+		try
+		{
+			Class.forName(driverName);
+			dbConn = DriverManager.getConnection(dbURL, userName, userPwd);
+			System.out.println(log+"Database Connect Success!");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.out.print(log+"Err:Database Connect Failure with "+e.toString());
+
+		}
+	    try {
+	        BufferedReader br = new BufferedReader(new InputStreamReader((ServletInputStream) request.getInputStream(), "utf-8"));
+	        StringBuffer sb = new StringBuffer("");
+	        String temp;
+	        while ((temp = br.readLine()) != null) {
+	            sb.append(temp);
+	        }
+	        br.close();
+	        acceptjson = sb.toString();
+	        JSONObject raw_JsonData = JSONObject.parseObject(acceptjson);
+	        String bus_id = raw_JsonData.getString("search");
+	        String businfo = "";
+			try {
+				String sql_bus = "select businfo from data_bus where busnumber = '"+bus_id+"'";
+				ResultSet rs_bus = null;
+				PreparedStatement stmt_bus = null;
+				stmt_bus = dbConn.prepareStatement(sql_bus);
+				rs_bus = stmt_bus.executeQuery();
+				while(rs_bus.next()) {
+					businfo = rs_bus.getString("businfo");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			JSONObject result = new JSONObject();  
+	        result.put("success", true);  
+	        result.put("businfo", businfo); 
+			
+			String sql = "select TOP(10) data_buspassenger.busnumber,data_buspassenger.timeline,sensorid,sensignal,senbattery,sentemp,senhumi,senco2,data_buspassenger.passenger from data_buspassenger,data_co2sensors where sensorid in (select sensnumber from data_bus where busnumber = '"+bus_id+"') and data_buspassenger.uuid=data_co2sensors.uuid order by timeline DESC ";
+	        JSONArray jsonArray = new JSONArray();
+	        try {
+				ResultSet rs = null;
+				PreparedStatement stmt = null;
+				stmt = dbConn.prepareStatement(sql);
+				rs = stmt.executeQuery();
+				int i=0;
+				while(rs.next()) {
+					 JSONObject sensor = new JSONObject();  
+					 sensor.put("busnumber", rs.getString("busnumber"));  
+					 sensor.put("timeline", rs.getString("timeline"));  
+				     sensor.put("sensorid", rs.getString("sensorid"));
+				     sensor.put("sensignal", rs.getString("sensignal"));
+				     sensor.put("senbattery", rs.getString("senbattery"));
+				     sensor.put("sentemp", rs.getString("sentemp"));
+				     sensor.put("senhumi", rs.getString("senhumi"));
+				     sensor.put("senco2", rs.getString("senco2"));
+				     sensor.put("passenger", rs.getString("passenger"));
+				     jsonArray.add(i++, sensor);
+				     System.out.println(log+""+i+""+jsonArray.toString());	
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	        
+	        result.put("data", jsonArray);
+	        
+	        String Message = result.toJSONString();
+	        String jsonStr = "{\"status\":\"success\"}";
+	        System.out.println(log+"POST:"+bus_id+", RETURN:"+jsonStr+"-"+Message);
+	        response.getWriter().write(Message);
+	    } catch (Exception e) {
+	    	String jsonStr = "{\"status\":\"error\"}";
+	    	response.getWriter().write(jsonStr);
+	        e.printStackTrace();
+	    } finally{
+	    	try {
+				dbConn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+	}
+}
